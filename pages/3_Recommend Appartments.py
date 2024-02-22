40fa725f8ae5aa55c8c127abc0f9ae0dae6f90c2
Suman Kumar Suman <samp.suman@gmail.com>
2024-02-22 10:44:12 +0530

    Update 3_Recommend Appartments.py
    Removed - Searches Within radius

--- pages/3_Recommend Appartments.py
+++ pages/3_Recommend Appartments.py
@@ -42,17 +42,17 @@ def recommend_properties_with_scores(property_name, top_n=5):
 recommend_properties_with_scores('DLF The Camellias')
 
 
-st.title('Select Location and Radius')
+# st.title('Select Location and Radius')
 
-selected_location = st.selectbox('Location',sorted(location_df.columns.to_list()))
+# selected_location = st.selectbox('Location',sorted(location_df.columns.to_list()))
 
-radius = st.number_input('Radius in Kms')
+# radius = st.number_input('Radius in Kms')
 
-if st.button('Search'):
-    result_ser = location_df[location_df[selected_location] < radius*1000][selected_location].sort_values()
+# if st.button('Search'):
+#     result_ser = location_df[location_df[selected_location] < radius*1000][selected_location].sort_values()
 
-    for key, value in result_ser.items():
-        st.text(str(key) + " " + str(round(value/1000)) + ' kms')
+#     for key, value in result_ser.items():
+#         st.text(str(key) + " " + str(round(value/1000)) + ' kms')
 
 st.title('Recommend Appartments')
 selected_appartment = st.selectbox('Select an appartment',sorted(location_df.index.to_list()))
